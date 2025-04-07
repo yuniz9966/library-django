a82e0e56d91ca366221abd6833656cd238646a2c
Pareiko Vladislav <parieiko.vlad@gmail.com>
2025-04-07 15:12:28 +0400

    Update admin panel

--- books/admin.py
+++ books/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext_lazy as _
 
-from books.models import Book, Author, AuthorBio  # импорт нужных моделей из нужного приложения
+from books.models import Book, Author, AuthorBio, User  # импорт нужных моделей из нужного приложения
 
 # в этом файле мы регистрируем те модели, которые нужны нам в работе в Админ панели по URL /admin
 
@@ -19,6 +22,28 @@ class BookAdmin(admin.ModelAdmin):
 #     list_per_page = 2
 
 
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    fieldsets = (
+        (None, {'fields': ('username', 'password', 'role')}),
+        (_('Personal info'), {'fields': ('first_name', 'last_name', 'email', 'phone', 'birth_day')}),
+        (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'password1', 'password2'),
+        }),
+    )
+    list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff', 'role')
+    search_fields = ('username', 'first_name', 'last_name', 'email')
+    ordering = ('username',)
+
+
+
+admin.site.unregister(Group)
+admin.site.register(Group)
 # admin.site.register(Book, BookAdmin)  # Регистрация модели книги
 admin.site.register(Author)  # Регистрация модели автора
 admin.site.register(AuthorBio)  # Регистрация модели профиля автора
