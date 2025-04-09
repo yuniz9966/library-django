02fd083eae75114ace330c270392ee61c6a832c5
Pareiko Vladislav <parieiko.vlad@gmail.com>
2025-04-09 12:55:17 +0400

    ORM Queries

--- queries.py
+++ queries.py
@@ -73,6 +73,163 @@ from books.models import Book
 # for book in books:
 #     print(book['title'], book['rating'])
 
-book = Book.objects.get(genre="qwerqwerqwer")
+# book = Book.objects.get(genre="qwerqwerqwer")
+#
+# print(book)
 
-print(book)
+
+# req_books = Book.objects.filter(
+#     rating=8.2,
+#     language="English"
+#
+# )
+#
+# print(req_books)
+
+# req_books = Book.objects.filter(
+#     rating__gt=8.2,
+#     language="English"
+# )
+#
+# print(req_books)
+
+
+# req_books = Book.objects.filter(
+#     title__icontains="ReaLitY"
+# )
+#
+# print(req_books)
+
+
+# req_books = Book.objects.filter(
+#     id__in=[2, 4, 6, 10]
+# )
+#
+# print(req_books)
+
+# req_books = Book.objects.filter(
+#     release_year__gt="2002-06-21"
+# )
+#
+# print(req_books)
+
+# req_books = Book.objects.filter(
+#     release_year__gte="2002-06-21"
+# )
+#
+# print(req_books)
+
+
+# req_books = Book.objects.filter(
+#     release_year__range=["2001-09-16", "2002-06-21"]
+# )
+#
+# print(req_books)
+
+
+# req_books = Book.objects.filter(
+#     title__istartswith="the"
+# )
+#
+# print(req_books)
+
+
+# req_books = Book.objects.filter(
+#     title__endswith="ing"
+# )
+#
+# print(req_books)
+
+
+# req_books = Book.objects.filter(
+#     title__endswith="ing"
+# )
+#
+# print(req_books)
+
+from django.db.models import Q
+
+# req_books = Book.objects.filter(
+#     genre="Psychology",
+#     release_year__gte="2002-09-06"
+# )
+#
+# print(req_books)
+
+
+# req_books = Book.objects.filter(
+#     (Q(genre="Psychology") & Q(release_year__gte="2002-09-06")) | Q(rating__gt=4)
+# )
+#
+# print(req_books)
+
+
+# books = Book.objects.filter(
+#     ~Q(language="French")
+# )
+#
+# print(books)
+#
+# for b in books:
+#     print(b.language)
+
+# from django.db.models import Q
+#
+# books = Book.objects.filter(
+#     (Q(genre="Fantasy") | Q(rating__lte=6)) & ~Q(release_year="2009-03-20")
+# )
+#
+# print(books)
+
+# =================================================================
+# =================================================================
+# =================================================================
+
+# book = Book.objects.get(id=152)
+# book.pages = 293
+#
+# book.save()
+
+
+# Book.objects.filter(genre="Business").update(
+#     rating=8.88
+# )
+
+
+# ==========================================================
+# ==========================================================
+# ==========================================================
+
+from django.db.models import F
+
+
+# Book.objects.filter(id__in=[1, 2, 3]).update(rating=F('rating') - 1)
+
+# Book.objects.filter(id__in=[1,]).update(title=F('title') + "( SSS)")
+
+# Book.objects.update(price=22.45)
+
+
+# Book.objects.update(
+#     discounted_price=F('price') * 0.8
+# )
+
+
+# Book.objects.get(title="Django Test ORM Query Result").delete()
+
+deleted, _ = Book.objects.filter(rating=1.1).delete()
+#
+# print(f"Удалено {deleted} записей -> {_}")
+
+# def delete():
+#     return count_deleted_objects, delete_details
+
+# def return_tuple() -> tuple[str, str]:
+#     return "FIRST", "SECOND"
+#
+# first, second = return_tuple()
+#
+# print(first, second)
+
+# for _ in range(10):
+#     print("HELLO")
